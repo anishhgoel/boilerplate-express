40bdd97f92984c1ea382421d189261917b5163d7
anishgoel@berkeley.edu <bobaslicer@gmail.com>
2024-11-14 00:57:47 -0800

    environment and some routes

--- myApp.js
+++ myApp.js
@@ -1,39 +1,24 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
+
+console.log("Hello World")
+
+app.use("/public", express.static(__dirname + "/public"))
+
+app.get('/', (req, res)=>{
+    res.sendfile(__dirname + "/views/index.html");
+})
 
 
 
+app.get("/json", (req,res) =>{
+    // let message = "Hello json";
+    // if (process.env.MESSAGE_STYLE === "uppercase"){\
+    //     message = message.toUpperCase();
+    // }
+    res.json({"message" : message});
+} )
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
